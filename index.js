2c726b3325b7e79e7b28df0dde16a14d304e63c6
buburuzaaaaa <110576807+buburuzaaaaa@users.noreply.github.com>
2022-09-06 16:33:13 -0700

    calc
    calc update

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function showWork(){
-    conwole.log(document.getElementById('inputs'))
-}
+
+//this is the code for the users input to binary
+let khadeem = parseInt(prompt("enter any number"))
+let bernard = khadeem.toString(2)
+// this is the hex answer for the users input
+let mec= khadeem.toString(16)
+alert('binary '+bernard)
+alert('hexadecimal '+ mec)
